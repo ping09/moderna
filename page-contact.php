5c49416c35dc3365a995ac7e8c1f32890f7e1272
pingstrom <pingstrom007@gmail.com>
2016-05-18 16:30:44 +0000

    added contactform to contact page

--- page-contact.php
+++ page-contact.php
@@ -44,37 +44,9 @@ get_header(); ?>
 		<div class="row">
 			<div class="col-lg-12">
 				<h4>Get in touch with us by filling <strong>contact form below</strong></h4>
-				<form id="contactform" action="contact/contact.php" method="post" class="validateform" name="send-contact">
-					<div id="sendmessage">
-						 Your message has been sent. Thank you!
-					</div>
-					<div class="row">
-						<div class="col-lg-4 field">
-							<input type="text" name="name" placeholder="* Enter your full name" data-rule="maxlen:4" data-msg="Please enter at least 4 chars" />
-							<div class="validation">
-							</div>
-						</div>
-						<div class="col-lg-4 field">
-							<input type="text" name="email" placeholder="* Enter your email address" data-rule="email" data-msg="Please enter a valid email" />
-							<div class="validation">
-							</div>
-						</div>
-						<div class="col-lg-4 field">
-							<input type="text" name="subject" placeholder="Enter your subject" data-rule="maxlen:4" data-msg="Please enter at least 4 chars" />
-							<div class="validation">
-							</div>
-						</div>
-						<div class="col-lg-12 margintop10 field">
-							<textarea rows="12" name="message" class="input-block-level" placeholder="* Your message here..." data-rule="required" data-msg="Please write something"></textarea>
-							<div class="validation">
-							</div>
-							<p>
-								<button class="btn btn-theme margintop10 pull-left" type="submit">Submit message</button>
-								<span class="pull-right margintop20">* Please fill all required form field, thanks!</span>
-							</p>
-						</div>
-					</div>
-				</form>
+				<?php if(have_posts()): while(have_posts()): the_post();?>
+					<?php the_content();?>
+				<?php endwhile; endif;?>
 			</div>
 		</div>
 	</div>
